d41099c03b1abd52f2993b59ef8aea819ffb925a
Fabien <fabienallouis@gmail.com>
2013-10-17 20:36:02 +0100

    Adding prototype methods to Word

--- js/main.js
+++ js/main.js
@@ -43,4 +43,30 @@ function Word(sandbox, word){
     }
 }
 
+Word.prototype = {
+    onKeyup: function(letter){
+        if(this.letters[letter] !== undefined) {
+            this.letters[letter] = true;
+            this.sandbox.emit("word:correct", letter);
+            if(this.isComplete()) {
+                this.sandbox.emit("word:complete");
+            }
+        } 
+    },
+    draw: function(ctx){
+        var word = this.word;
+        this.letters.forEach(function(letter, i){
+            if(letter===false) word = word.split(numToLetter(i)).join("_");
+        });
+        ctx.fillStyle = "#75C010";
+        ctx.fillText(word, 10, 200);
+    },
+    isComplete: function(){
+        return this.letters.every(function(letter){ return letter; }); 
+    },
+    toString: function(){
+        return this.word.split(" ").join("");
+    }
+};
+
 }
