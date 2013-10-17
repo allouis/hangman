b44ff0e88fe5c9850f09eee1af58a1baff9da6b5
Fabien O'Carroll <fabien@Fabiens-Mac-mini.local>
2013-10-17 13:38:03 +0100

    Adding Alphabet constructor
    Implements the GameObject constructor.
    Fills letters array with booleans, whether letter
    has been picked or not.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ function GameObject(sandbox){
     sandbox.on("game:keyup", this.onKeyup, this);
     sandbox.on("game:draw", this.draw, this);
 }
+
+// Alphabet
+function Alphabet(sandbox){
+    GameObject.apply(this, arguments);
+    this.pickedLetters = [];
+    for(var i = 0, l = 26; i < l; i++){
+        this.letters[i] = false;
+    }
+    this.sandbox.on("word:correct", this.removeLetter, this);
+}
