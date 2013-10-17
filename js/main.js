15a90dce6e9175e86630cab3e7289c754ff1f8f4
Fabien <fabienallouis@gmail.com>
2013-10-17 20:32:05 +0100

    Adding prototype methods to Alphabet

--- js/main.js
+++ js/main.js
@@ -17,6 +17,20 @@ function GameObject(sandbox){
 // Alphabet
 function Alphabet(sandbox){
     GameObject.apply(this, arguments);
+}
+
+Alphabet.prototype = {
+    onKeyup: function(letter){
+        if(this.letters.indexOf(letter)<0)this.letters.push(letter);
+    },
+    draw:function(ctx){
+        ctx.fillStyle = "#C01075";
+        ctx.fillText(this.getText(), 10, 50);
+    },
+    getText:function(){
+        return this.letters
+                .map(function(n){ return numToLetter(n); })
+                .join(" ");
     }
     this.sandbox.on("word:correct", this.removeLetter, this);
 }
