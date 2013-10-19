3d80085a68cdef58763c5bfe542700624e992974
Fabien <fabienallouis@gmail.com>
2013-10-19 14:57:31 +0100

    Fixing bug of ridiulously long phrases

--- js/Word.js
+++ js/Word.js
@@ -22,8 +22,16 @@ Word.prototype = {
         this.letters.forEach(function(letter, i){
             if(letter===false) word = word.split(numToLetter(i)).join("_");
         });
+        var lines = [word];
+        if(ctx.measureText(word).width > 600){
+            var words = word.split("  ");
+            var mid = words.length/2 + 1;
+            lines = [words.splice(0, mid).join("  "), words.join("  ").substring(1)];
+        }
         ctx.fillStyle = "#75C010";
-        ctx.fillText(word, 10, 200);
+        for(var i = 0, l = lines.length; i < l; i++){
+            ctx.fillText(lines[i], 10, 200 + (30 *i));
+        }
     },
     isComplete: function(){
         return this.letters.every(function(letter){ return letter; }); 
